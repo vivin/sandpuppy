867027e411540e4312d091b5e71b5251d1ba54c2
Vivin Paliath <vivin.paliath@gmail.com>
2021-01-26 21:58:26 -0700

    small fuzzing time for testing.

--- scripts/exp.pl
+++ scripts/exp.pl
@@ -137,7 +137,7 @@ my $log = Log::Simple::Color->new;
               $SIG{INT} = 'IGNORE';
 
               my $STARTUP_TIME = 10; # about the time it takes to start up vvdproc and the fuzzer
-              my $FUZZ_TIME = 300 + $STARTUP_TIME;
+              my $FUZZ_TIME = 120 + $STARTUP_TIME;
               my $killed = 0;
               my $start_time = time();
               my $fuzzer_pid = &{$tasks->{fuzz}}($experiment_name, $subject, $version, $context, $waypoints, $binary_context, 0);
