b317818f25cb86ff3e79adc660d0466b6fd97a34
Vivin Paliath <vivin.paliath@gmail.com>
2022-01-27 18:40:37 -0700

    provide some feedback

--- scripts/all-smbc-findpos.pl
+++ scripts/all-smbc-findpos.pl
@@ -10,10 +10,16 @@ if (! -d $BASE_PATH) {
 my $output_file = "$BASE_PATH/vivin/smartdsf/smbc/results/max_world_pos.txt";
 while (1) {
   foreach my $level(0, 2, 3, 4, 5, 7) {
+    print "Level: $level\n";
+
     foreach my $run(1, 2, 3, 4, 5) {
+      print " Run: $run\n";
+
       system "scripts/smbc-find-max-world-pos smartdsf $level l$level-run-$run >> $output_file.tmp 2>&1";
       system "scripts/smbc-find-max-world-pos smartdsf $level l$level-vanilla-run-$run >> $output_file.tmp 2>&1";
     }
+
+    print "\n";
   }
 
   system "mv $output_file.tmp $output_file";
