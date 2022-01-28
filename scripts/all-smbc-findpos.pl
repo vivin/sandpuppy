342eff1d9016ccf11c9e12c9278e28bff1d366d5
Vivin Paliath <vivin.paliath@gmail.com>
2022-01-27 18:38:50 -0700

    Write out to file from script. Use a temp file so that the output file does not keep growing.

--- scripts/all-smbc-findpos.pl
+++ scripts/all-smbc-findpos.pl
@@ -2,13 +2,20 @@
 use strict;
 use warnings FATAL => 'all';
 
+my $BASE_PATH = "/mnt/vivin-nfs";
+if (! -d $BASE_PATH) {
+  $BASE_PATH = "/media/2tb/phd-workspace/nfs";
+}
+
+my $output_file = "$BASE_PATH/vivin/smartdsf/smbc/results/max_world_pos.txt";
 while (1) {
   foreach my $level(0, 2, 3, 4, 5, 7) {
     foreach my $run(1, 2, 3, 4, 5) {
-      system "scripts/smbc-find-max-world-pos smartdsf $level l$level-run-$run";
-      system "scripts/smbc-find-max-world-pos smartdsf $level l$level-vanilla-run-$run";
+      system "scripts/smbc-find-max-world-pos smartdsf $level l$level-run-$run >> $output_file.tmp 2>&1";
+      system "scripts/smbc-find-max-world-pos smartdsf $level l$level-vanilla-run-$run >> $output_file.tmp 2>&1";
     }
   }
 
+  system "mv $output_file.tmp $output_file";
   sleep 600;
 }
