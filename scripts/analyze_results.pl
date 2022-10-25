53cdb233b9b3d4f5899c60939fef56fe39946f7e
Vivin Paliath <vivin.paliath@gmail.com>
2022-10-25 10:38:46 -0700

    test multithreaded analysis.

--- scripts/analyze_results.pl
+++ scripts/analyze_results.pl
@@ -45,11 +45,11 @@ my $pool = Thread::Pool->new({
     do           => sub {
         my $session = $_[0];
         my $input_file = $_[1];
+        print "\nrunning job for session $session, input file $input_file\n";
         return $session, $input_file, analysis::get_basic_blocks_for_input($subject, $input_file);
     },
     stream       => sub {
-        shift;
-        print "\nstream called for for session $_[0] and file $_[1]\n";
+        print "\nstream called for session $_[0] and file $_[1]\n";
         if ($_[0] eq "__COMPLETED__") {
             $queue->end();
         } else {
@@ -83,6 +83,7 @@ sub iteration_handler {
     my $session = $_[0];
     my $input_file = $_[1];
     $pool->job($session, $input_file);
+    print "\nsubmitted job for session $session, input $input_file\n";
 }
 
 sub process_file_with_coverage_data {
