b892c0ad43301433b5eb593583fa8159415785d9
Vivin Paliath <vivin.paliath@gmail.com>
2022-10-26 00:29:56 -0700

    fix

--- scripts/bg_analyze_results.sh
+++ scripts/bg_analyze_results.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-EXISTING_PID=$(ps -u | grep -v grep | grep "analyze_results.pl $1 $2 $3 $4" | awk '{ print \$2; }')
+EXISTING_PID=$(ps -u | grep -v grep | grep "analyze_results.pl $1 $2 $3 $4" | awk '{ print $2; }')
 if [[ -z "$EXISTING_PID" ]]; then
   /home/vivin/Projects/phd/scripts/analyze_results.pl "$1" "$2" "$3" "$4" > /dev/null 2>&1 &
 fi
