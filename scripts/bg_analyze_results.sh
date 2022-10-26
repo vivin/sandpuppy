5f81eae1166d163e9221ae18c3af54ed3484dbaf
Vivin Paliath <vivin.paliath@gmail.com>
2022-10-26 01:02:10 -0700

    nohup launch

--- scripts/bg_analyze_results.sh
+++ scripts/bg_analyze_results.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-EXISTING_PID=$(ps -au | grep -v grep | grep "analyze_results.pl $1 $2 $3 $4" | awk '{ print $2; }')
+EXISTING_PID=$(ps -ux | grep -v grep | grep "analyze_results.pl $1 $2 $3 $4" | awk '{ print $2; } | head -1')
 if [[ -z "$EXISTING_PID" ]]; then
   nohup /home/vivin/Projects/phd/scripts/analyze_results.pl "$1" "$2" "$3" "$4" > /dev/null 2>&1 &
 else
