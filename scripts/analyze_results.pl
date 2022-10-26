8e8d5fc97df51fe674a05660e4d4a77f6836df91
Vivin Paliath <vivin.paliath@gmail.com>
2022-10-25 21:06:18 -0700

    testing multiprocessing

--- scripts/analyze_results.pl
+++ scripts/analyze_results.pl
@@ -73,9 +73,9 @@ my $pool = Thread::Pool->new({
         #}
     #},
     autoshutdown => 1,
-    workers      => 128,
-    maxjobs      => 640,
-    minjobs      => 320,
+    workers      => 32,
+    maxjobs      => 160,
+    minjobs      => 80,
 });
 
 #my $worker = threads->create(
