89c15c3216ec1d348f248ef04b326d8997726fad
Vivin Paliath <vivin.paliath@gmail.com>
2022-10-27 15:39:15 -0700

    redis backup script

---
+++ scripts/backup_redis.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+
+system "mkdir -p /private-nfs/vivin/redis-backup";
+while (1) {
+    system "cp -v /redis/*.rdb /private-nfs/vivin/redis-backup";
+    sleep 300;
+}
